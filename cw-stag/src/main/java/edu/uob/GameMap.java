c0e2c3575f988c9353fc9bcae331003081195d6c
zbhbarry <xr23370@bristol.ac.uk>
2024-05-08 19:30:22 +0100

    Update GameMap.java

--- cw-stag/src/main/java/edu/uob/GameMap.java
+++ cw-stag/src/main/java/edu/uob/GameMap.java
@@ -6,27 +6,88 @@ import com.alexmerz.graphviz.objects.Node;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public final class GameMap {
     private Location startLocation;
-    private HashMap<String, Location> mapIndexes;
+    private final HashMap<String, Location> mapIndexes;
+    private HashSet<String> entriesAll;
+    private Location storeRoom;
 
     public GameMap(ArrayList<Graph> sections) {
         ArrayList<Graph> locations = sections.get(0).getSubgraphs();
         ArrayList<Edge> paths = sections.get(1).getEdges();
         this.startLocation = null;
+        this.storeRoom = null;
         mapIndexes = new HashMap<>();
+        entriesAll = new HashSet<>();
+        createMapIndexes(locations, paths);
+    }
+
+    public Location getStartLocation() {
+        return startLocation;
+    }
+
+    public Location getStoreRoom() {
+        return storeRoom;
+    }
+
+    public HashSet<String> getEntriesAll() {
+        return entriesAll;
+    }
+
+    public Set<String> getLocations() {
+        return mapIndexes.keySet();
+    }
+
+    public HashMap<String, Location> getMapIndexes() {
+        return mapIndexes;
     }
 
     private Location createLocation(Graph location) {
         Node locationDetails = location.getNodes(false).get(0);
         String locationName = locationDetails.getId().getId();
-        return new Location(locationName, null);
-        // TODO
+        String description = locationDetails.getAttribute("description");
+        Location loc = new Location(locationName, description);
+        this.entriesAll.add(locationName);
+
+        // Add characters, artefacts, furniture
+        for (Graph subgraph : location.getSubgraphs()) {
+            for (Node item : subgraph.getNodes(false)) {
+                String itemName = item.getId().getId();
+                this.entriesAll.add(itemName);
+                String itemDesc = item.getAttribute("description");
+                switch (subgraph.getId().getId()) {
+                    case "characters":
+                        loc.addCharacter(new Character(itemName, itemDesc));
+                        break;
+                    case "artefacts":
+                        loc.addArtefact(new Artefact(itemName, itemDesc));
+                        break;
+                    case "furniture":
+                        loc.addFurniture(new Furniture(itemName, itemDesc));
+                        break;
+                }
+            }
+        }
+
+        // handle storeRoom
+        if (locationName.equals("storeroom")) {
+            this.storeRoom = loc;
+        }
+
+        return loc;
     }
 
     private void addPath(Edge path) {
-        // TODO
+        String fromName = path.getSource().getNode().getId().getId();
+        String toName = path.getTarget().getNode().getId().getId();
+        Location fromLoc = mapIndexes.get(fromName);
+        Location toLoc = mapIndexes.get(toName);
+        if (fromLoc != null && toLoc != null) {
+            fromLoc.addPath(toLoc);
+        }
     }
 
     private void createMapIndexes(ArrayList<Graph> locations, ArrayList<Edge> paths) {
@@ -40,5 +101,10 @@ public final class GameMap {
         Node firstNode = paths.get(0).getSource().getNode();
         String fromName = firstNode.getId().getId();
         this.startLocation = mapIndexes.get(fromName);
+        if (this.storeRoom == null) {
+            this.storeRoom = new Location("storeroom", "storeroom");
+            mapIndexes.put("storeroom", this.storeRoom);
+        }
+        this.entriesAll.add("health");
     }
 }
